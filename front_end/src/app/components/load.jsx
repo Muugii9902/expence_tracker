e7b66a973ac306111ea8be1edd3313ce0cbd965d
Muugii9902 <muugii9902@gmail.com>
2024-09-01 19:28:50 +0800

    records

--- front_end/src/app/components/load.jsx
+++ front_end/src/app/components/load.jsx
@@ -6,7 +6,10 @@ const Load = () => {
       <div className="flex flex-col justify-center items-center">
         <div>
           <img src="./zurag/logo.png" alt="logo" />
-          <span className="loading loading-spinner loading-lg"></span>
+          <span className="loading loading-spinner loading-lg">
+            {" "}
+            ta tur huleene uu
+          </span>
         </div>
       </div>
     </div>
