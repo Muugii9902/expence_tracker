d25b00a53b99fa8ad3f90b1505837f7c7ebac84d
Muugii9902 <muugii9902@gmail.com>
2024-09-03 19:25:05 +0800

    signup404

--- back_end/routes/user-route.js
+++ back_end/routes/user-route.js
@@ -1,13 +1,17 @@
 const { Router } = require("express");
 const {
+  getCurrentUser,
   getAllUser,
   createUser,
   updateUser,
   deleteUser,
 } = require("../controllers/user-controllers");
 
+const {auth}=require("../middlewares/auth")
+
 const router = Router();
 
+router.route("/profile").get(auth,getCurrentUser);
 router.route("/").get(getAllUser).post(createUser);
 router.route("/:id").put(updateUser).delete(deleteUser);
 
--- front_end/src/app/page.js
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import LogIn from "./components/login";
-import Load from "./components/load/page";
-
-export default function Home() {
-  return <main></main>;
-}
