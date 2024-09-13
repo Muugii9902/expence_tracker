2e68bc4de025f59239a5a022e150117ddd65ed2e
Muugii9902 <muugii9902@gmail.com>
2024-09-13 12:33:48 +0800

    vercel

--- back_end/config/postgres.js
+++ back_end/config/postgres.js
@@ -1,15 +1,15 @@
-const postgres = require("postgres")
+const postgres = require("postgres");
 
 const sql = postgres({
-    host: 'ep-mute-fog-a57rimof.us-east-2.aws.neon.tech',
-    database: 'expense_db',
-    username: 'expense_db_owner',
-    password: 'BHwm2DzOFy4L',
-    port: 5432,
-    ssl: 'require',
-    connection: {
-      options: `project=ep-mute-fog-a57rimof`,
-    },
-  });
+  host: "GHOST",
+  database: "PGDATABASE",
+  username: "PGUSER",
+  password: "PGPASSWORD",
+  port: 5432,
+  ssl: "require",
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
 
-  module.exports=sql
+module.exports = sql;
